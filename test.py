0c32fbae77996149fa06fd0dd70898ffccddd0e3
xiaoxixiao <58674030+xiaoxixiao@users.noreply.github.com>
2023-08-13 18:12:37 +0800

    xml方法测试

--- test.py
+++ test.py
@@ -7,63 +7,76 @@ logger = logging.getLogger(__name__)
 
 
 def parse_xml(input_data):
-    """
-    解析给定的XML文件路径或XML字符串，并遍历每一层的节点和属性。
-
-    参数:
-        input_data (str): 要解析的XML文件路径或XML字符串。
-
-    返回:
-        dict: 字典，其键是整数，表示层数（从0开始），值是包含以下键的字典：
-              - '节点': 包含该层所有节点名称的列表。
-              - '属性': 包含该层所有节点属性的列表。每个元素是由属性名称和属性值组成的键值对列表。
-    """
-
-    def traverse_tree(node, level):
-        if level not in result:
-            result[level] = {'节点': [], '属性': []}
-
-        # 存储节点名称和属性
-        result[level]['节点'].append(node.tag)
-        result[level]['属性'].append(list(node.attrib.items()))
-
-        # 遍历子节点
-        for child in node:
-            traverse_tree(child, level + 1)
-
     try:
-        # 尝试解析XML字符串
         root = ET.fromstring(input_data)
+        return root
     except ET.ParseError:
         try:
-            # 如果解析字符串失败，则尝试作为文件路径解析
             tree = ET.parse(input_data)
             root = tree.getroot()
+            return root
         except ET.ParseError as e:
             logger.error(f'解析XML时出错: {e}')
             return None
 
     result = {}
 
-    # 递归函数，用于遍历XML
+    def traverse_tree(node, level):
+        if level not in result:
+            result[level] = {'节点': [], '属性': []}
+
+        # 存储节点名称
+        result[level]['节点'].append(node.tag)
+
+        # 将属性存储为字典
+        attributes_dict = {attr: value for attr, value in node.attrib.items()}
+        result[level]['属性'].append(attributes_dict)
+
+        for child in node:
+            traverse_tree(child, level + 1)
 
     traverse_tree(root, 0)
     logger.info('XML解析成功')
     return result
 
 
-if __name__ == "__main__":
-    xml_string = '''<root>
-        <child1 attribute1="value1" attribute2="value2">text1</child1>
-        <child2 attribute3="value3">text2</child2>
-        <child3>
-            <grandchild attribute4="value4">text3</grandchild>
-        </child3>
-    </root>'''
+def find_paths_to_level(root, target_level, selected_node, selected_attribute_value):
+    paths_with_attributes = []
 
-    parsed_xml = parse_xml(xml_string)
-    if parsed_xml:
-        for level, data in parsed_xml.items():
-            print(f"层数: {level}, 节点: {data['节点']}, 属性: {data['属性']}")
-    else:
-        print('解析XML时出错')
+    def traverse_tree(node, current_level, current_path, current_attributes):
+        # 如果到达目标层级，将当前路径和属性添加到结果中
+        if current_level == target_level:
+            current_attributes.append({attr_key: attr_value for attr_key, attr_value in node.attrib.items()})
+            path_str = '/'.join(current_path)
+            attr_key, attr_value = selected_attribute_value.split('=')
+            if selected_node in path_str and any(attr.get(attr_key) == attr_value for attr in current_attributes):
+                paths_with_attributes.append((path_str, current_attributes.copy()))
+            current_attributes.pop()
+
+        # 递归遍历子节点
+        for child in node:
+            current_path.append(child.tag)
+            current_attributes.append({attr_key: attr_value for attr_key, attr_value in child.attrib.items()})
+            traverse_tree(child, current_level + 1, current_path, current_attributes)
+            current_path.pop()
+            current_attributes.pop()
+
+    traverse_tree(root, 0, [root.tag], [])
+    return paths_with_attributes
+
+
+if __name__ == "__main__":
+
+    xml_path = r"D:\.game\steam\steamapps\common\7 Days To Die\Data\Config\items.xml"
+
+    root = parse_xml(xml_path)
+    selection = {
+        '所选层': 2,
+        '所选节点': 'property',
+        '所选属性值': 'name=Tags'
+    }
+
+    paths_to_level = find_paths_to_level(root, selection['所选层'], selection['所选节点'], selection['所选属性值'])
+    for path, attributes in paths_to_level:
+        print(f"路径: {path}")
+        print(f"属性: {attributes}")
